7e6a8d7fbb2ad0d4cd18d9d8f705d02f9f03bfa2
sunset-choe <>
2022-04-18 10:33:42 +0900

    spring-j: 웹소켓 애플리케이션 작성

---
+++ java/spring-j/reactive-websocket/server/src/main/java/sunset/reactive/websocket/EchoWebSocketHandler.java
@@ -0,0 +1,19 @@
+package sunset.reactive.websocket;
+
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.WebSocketSession;
+import reactor.core.publisher.Mono;
+
+public class EchoWebSocketHandler implements WebSocketHandler {
+
+    @Override
+    public Mono<Void> handle(WebSocketSession session) {
+        return session.receive()
+            .map(WebSocketMessage::getPayloadAsText)
+            .map(tm -> "Echo: " + tm)
+            .log()
+            .map(session::textMessage)
+            .as(session::send);
+    }
+}
