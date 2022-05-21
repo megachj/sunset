f8ca24a55490d53372cc943f4c3f1fca318fd534
sunset-choe <>
2022-05-21 18:55:35 +0900

    .

--- java/spring-j/reactive/reactor/src/test/java/sunset/reactive/reactor/T04_에러처리.java
+++ java/spring-j/reactive/reactor/src/test/java/sunset/reactive/reactor/T04_에러처리.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 import java.time.Duration;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -12,8 +13,9 @@ import reactor.test.StepVerifier;
 @Slf4j
 public class T04_에러처리 {
 
+    @DisplayName("just 에서 예외가 던져지면 error 신호가 아니라 그냥 예외가 던져진다.")
     @Test
-    public void just_내부에서_예외가_발생했을때() {
+    public void just_throws() {
         String exceptionMessage = "just는 퍼블리셔를 생성할 때 데이터를 만든다.";
         Throwable thrown = catchThrowable(() -> {
             Flux.just(throwException(exceptionMessage));
@@ -27,18 +29,19 @@ public class T04_에러처리 {
         throw new IllegalStateException(exceptionMessage);
     }
 
+    @DisplayName("flatMap 에서 예외가 던져지면 바깥 스트림에 error 신호가 발생한다.")
     @Test
-    public void flatMap에서_예외가_던져졌을때() {
+    public void flatMap_throws() {
         Flux<Integer> publisher = Flux.<Integer>create(sink -> {
                 sink.next(1);
                 sink.next(-1);
                 sink.next(2);
             })
             .flatMap(integer -> toIntegerSequenceWithThrows(integer)
-                .log("FlatMap 내부 Flux")
+                .log("InnerFlatMapRxStream")
                 .onErrorResume(e -> Mono.empty())
             )
-            .log("Main Flux");
+            .log("MainRxStream");
 
         StepVerifier.create(publisher)
             .expectNext(1)
@@ -53,18 +56,19 @@ public class T04_에러처리 {
         return Flux.range(1, i);
     }
 
+    @DisplayName("flatMap 에서 error 신호를 발생시키고 그걸 잡으면, 바깥 스트림에는 해당 error 신호가 발생하지 않게 할 수 있다.")
     @Test
-    public void flatMap에서_에러신호가_발생했을때() {
+    public void flatMap_error_resume() {
         Flux<Integer> publisher = Flux.<Integer>create(sink -> {
                 sink.next(1);
                 sink.next(-1);
                 sink.next(2);
             })
             .flatMap(integer -> toIntegerSequenceWithErrorSignal(integer)
-                .log("FlatMap 내부 Flux")
+                .log("InnerFlatMapRxStream")
                 .onErrorResume(e -> Mono.empty())
             )
-            .log("Main Flux");
+            .log("MainRxStream");
 
         StepVerifier.create(publisher)
             .expectNext(1)
