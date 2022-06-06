a3cf4de345e57a4856efc0fb2f6ab0e6a6ab0d45
sunset-choe <>
2022-06-06 12:10:44 +0900

    websocket-server: blockHound 라이브러리 적용

--- java/spring-j/reactive/websocket-server/build.gradle.kts
+++ java/spring-j/reactive/websocket-server/build.gradle.kts
@@ -1,7 +1,10 @@
 apply(from = "../../docker-build.gradle")
 
+val blockHoundVersion: String by ext
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("io.projectreactor.tools:blockhound:${blockHoundVersion}")
 
     testImplementation("io.projectreactor:reactor-test")
 }
